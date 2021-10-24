9ee67795786b9edaecbfcabb0378163de4d5bac4
Beatriz Beato <beatrizbeatosilva@gmail.com>
2021-10-24 19:52:04 +0100

    basic structure-styles commit

--- _site/assets/js/base.js
+++ _site/assets/js/base.js
@@ -1,3 +1,27 @@
 // ---
+$( document ).ready(function() {
+    console.log('ready!');
+
+
+    function disableScroll(){
+      console.log('finalmente!')
+    }
+
+    disableScroll();
+
+
+     $('.js-play-sound').hover(function(){
+      console.log('fale')
+        $("#nav_audio")[0].play();
+        },
+    function(){
+        $("#nav_audio")[0].load();
+    });
+
+   
+
+
+});
 // ---
+
 
