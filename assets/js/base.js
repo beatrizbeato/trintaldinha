fe8469d9b7b6181f06a91790621152c7e8211eeb
Beatriz Beato <beatrizbeatosilva@gmail.com>
2021-10-26 13:34:24 +0100

    improve flipbook dresscode mobiçe

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,9 +3,15 @@ $( document ).ready(function() {
     console.log('ready!');
 
     // init graphs and charts
-    if ($('#magazine').length ) {
-      $('#magazine').turn({ acceleration: true});
-    }
+  if ($('#magazine').length ) {
+    var flipbookEL = document.getElementById('magazine');
+    window.addEventListener('resize', function (e) {
+      flipbookEL.style.width = '';
+      flipbookEL.style.height = '';
+      $(flipbookEL).turn('size', flipbookEL.clientWidth, flipbookEL.clientHeight);
+    });
+    $(flipbookEL).turn();
+  }
 
     $("#nav_audio")[0].load();
 
