78898df4219b9691b8cad5d0f259bcd5b08fc441
Beatriz Beato <beatrizbeatosilva@gmail.com>
2021-10-26 17:23:26 +0100

    add sounds + minor fixes

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,8 +1,7 @@
 // ---
 $( document ).ready(function() {
-    console.log('ready!');
 
-    // init graphs and charts
+  // init graphs and charts
   if ($('#magazine').length ) {
     var flipbookEL = document.getElementById('magazine');
     window.addEventListener('resize', function (e) {
@@ -13,12 +12,64 @@ $( document ).ready(function() {
     $(flipbookEL).turn();
   }
 
-    $("#nav_audio")[0].load();
+  if ($('#hero-audio').length ) {
+    $("#hero-audio")[0].load();
+    $("#dresscode-audio")[0].load();
+    $("#map-audio")[0].load();
+    $("#dormidas-audio")[0].load();
+  }
 
-     $('.js-play-sound').hover(function(){
-        $("#nav_audio")[0].play();
-        },
-    );
+
+  function playAudio(element) {
+    element.play();
+    element.animate({volume: 1}, 1000);
+  }
+
+  function pauseAudio(element) {
+    element.animate({volume: 0}, 1000);
+    element.pause();
+  }
+
+  $( ".js-play-hero-audio").mouseenter(function() {
+      var selectedAudio = $("#hero-audio")[0]
+      playAudio(selectedAudio)
+  });
+
+  $( ".js-play-dresscode-audio" ).mouseenter(function() {
+      var selectedAudio = $("#dresscode-audio")[0]
+      playAudio(selectedAudio)
+  });
+
+  $( ".js-play-map-audio" ).mouseenter(function() {
+      var selectedAudio = $("#map-audio")[0]
+      playAudio(selectedAudio)
+  });
+
+  $( ".js-play-dormidas-audio" ).mouseenter(function() {
+      var selectedAudio = $("#dormidas-audio")[0]
+      playAudio(selectedAudio)
+  });
+
+  $( ".js-play-hero-audio").mouseleave(function() {
+      var selectedAudio = $("#hero-audio")[0]
+      pauseAudio(selectedAudio)
+  });
+
+  $( ".js-play-dresscode-audio" ).mouseleave(function() {
+      var selectedAudio = $("#dresscode-audio")[0]
+      pauseAudio(selectedAudio)
+  });
+
+  $( ".js-play-map-audio" ).mouseleave(function() {
+      var selectedAudio = $("#map-audio")[0]
+      pauseAudio(selectedAudio)
+  });
+
+  $( ".js-play-dormidas-audio" ).mouseleave(function() {
+      var selectedAudio = $("#dormidas-audio")[0]
+      pauseAudio(selectedAudio)
+  });
+
 
 });
 // ---
